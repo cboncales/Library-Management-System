ae268920c96ba8b8571c6267d3ab86815b5a703c
cboncales <clarkboncales115@gmail.com>
2024-12-14 17:55:32 +0800

    feat: setup django backend and postgres as database

---
+++ backend/library/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+class Role(models.Model):
+    role_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.role_name
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    genre = models.CharField(max_length=50)
+    available_copies = models.IntegerField()
+
+class BorrowRecord(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrow_date = models.DateField(auto_now_add=True)
+    return_date = models.DateField(null=True, blank=True)
+
